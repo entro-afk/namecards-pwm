c642734538a49e0595d9755c739a3ac3364f0d38
Ana Calabano <acalabano@circleblack.com>
2020-12-12 21:32:22 -0500

    app maker profile

--- commands/appmaker.js
+++ commands/appmaker.js
@@ -32,7 +32,7 @@ module.exports = {
     let targetUser = requestingUser;
     if (args) {
       if (!isNaN(args[0])) {
-        if (args[0] >= 20) {
+        if (args[0].length >= 20) {
           targetUser = {
             discordID: null,
             characterID: args[0]
